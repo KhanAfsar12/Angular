590bb554235d6fe405defe4c4346a6a8c8e14aab
KhanAfsar12 <ka484564@gmail.com>
2024-01-18 14:44:13 +0530

    service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
+import { MyserviceService } from './myservice.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Afsar Khan';
+  public choose = '';
+  setvalue(drp:any){
+    this.choose=drp.target.value;
+  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CompanyComponent } from './company/company.component';
+// import { CompanyComponent } from './company/company.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { CompanyModule } from './company/company.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompanyComponent,
     EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CompanyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(){
+    console.log("Hello World");
+  }
+ }
