39306ec97b9a88b723db19a2a183e07d5a643f8b
KhanAfsar12 <ka484564@gmail.com>
2024-01-19 15:20:36 +0530

    service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,115 @@ import { MyserviceService } from './myservice.service';
 })
 export class AppComponent {
   title = 'Afsar Khan';
-  public choose = '';
-  setvalue(drp:any){
-    this.choose=drp.target.value;
-  }
+  // Students:any[] = [
+  //   {'name':'Afsar Khan'},
+  //   {'name':'Ishtiyaque Hussain'},
+  //   {'name':'Ashraf Khan'},
+  //   {'name':'Kaif Siddique'}
+  // ]
+  // public choose = '';
+  // setvalue(drp:any){
+  //   this.choose=drp.target.value;
+  // }
  
+  // students:any[] = [];
+  // constructor(){
+  //   this.students=[
+  //     {
+  //       studentId:1,
+  //       name:'Afsar Khan',
+  //       gender:'Male',
+  //       course:'Bsc.IT',
+  //       age:20
+  //     },
+  //     {
+  //       studentId:2,
+  //       name:'kaif Siddique',
+  //       gender:'Male',
+  //       course:'Bsc.IT',
+  //       age:20
+  //     },
+  //     {
+  //       studentId:3,
+  //       name:'Ahmed Khan',
+  //       gender:'Male',
+  //       course:'MCA',
+  //       age:23
+  //     },
+  //     {
+  //       studentId:4,
+  //       name:'Fiasal',
+  //       gender:'Male',
+  //       course:'Bsc',
+  //       age:22
+  //     }
+  //   ];
+  // }
+  // getMoreStudents():void{
+  //   this.students=[
+  //     {
+  //       studentId:1,
+  //       name:'Afsar Khan',
+  //       gender:'Male',
+  //       course:'Bsc.IT',
+  //       age:20
+  //     },
+  //     {
+  //       studentId:2,
+  //       name:'kaif Siddique',
+  //       gender:'Male',
+  //       course:'Bsc.IT',
+  //       age:20
+  //     },
+  //     {
+  //       studentId:3,
+  //       name:'Ahmed Khan',
+  //       gender:'Male',
+  //       course:'MCA',
+  //       age:23
+  //     },
+  //     {
+  //       studentId:4,
+  //       name:'Fiasal',
+  //       gender:'Male',
+  //       course:'Bsc',
+  //       age:22
+  //     },
+  //     {
+  //       studentId:5,
+  //       name:'Huzaifa',
+  //       gender:'Male',
+  //       course:'BBA',
+  //       age:20
+  //     }
+  //   ];
+  // }
+  // TrackByStudentId(index:number, student:any):string{
+  //   return student.studentId;
+  // }
+
+
+  constructor(){
+  }
+
+  CountryDetails:any[]=[
+    {
+      'country':'India',
+      'people' : [
+        {'name':"Afsar Khan"},
+        {'name':'Abrar Khan'},
+        {'name':'Farhan'}
+    ]
+    },
+
+    {
+      'country':'Saudi arabia',
+      'people' : [
+        {'name':"Usama bin laden"},
+        {'name':'King Faisal'},
+        {'name':'Ibrahim'}
+    ]
+    }
+  ];
+
 }
