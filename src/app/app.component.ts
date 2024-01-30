0c1a929e67edafad6c38cdccd21690cfdbc39927
KhanAfsar12 <ka484564@gmail.com>
2024-01-30 13:00:19 +0530

    service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,17 @@ import { MyserviceService } from './myservice.service';
 export class AppComponent {
   title = 'Afsar Khan';
  
+  dob = new Date(2004, 5, 6);
+  salary:number = 65000;
+
+  name:string = "Afsar Khan";
+  work:string = "software engineer";
+  position:string = "Data Engineer";
+
+  currentDate = new Date();
+  myNumber:number = 0.1234567893455;
   constructor(){
   }
-data:string="Afsar";
-data1:string="Ishtiyaque";
 
 }
 
